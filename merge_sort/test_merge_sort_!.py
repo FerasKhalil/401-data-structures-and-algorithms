94c243df0302fcd122287eaa7a0b508284dd7ca4
FIRAS AHMAD HASAN <x.firashasan@gmail.com>
2021-08-31 06:35:30 +0300

    merge sort function is done

---
+++ merge_sort/test_merge_sort_!.py
@@ -0,0 +1,8 @@
+from merge_sort.merge_sort_1 import *
+
+def test_merge_sort():
+    lister = [8, 4, 23, 42, 16, 15]
+    merge_sort(lister)
+    actual = lister
+    expected = [4, 8, 15, 16, 23, 42]
+    assert actual == expected
