e338a86a75c564c397d663b212907a9e7d0be4d7
FIRAS AHMAD HASAN <x.firashasan@gmail.com>
2021-09-04 23:09:49 +0300

    hash table CC30 complete

--- hash_table/README.md
+++ hash_table/README.md
@@ -1,11 +1,38 @@
 # Hashtables
 <!-- Short summary or background information -->
+Hash tables allow the storage and retrieval of data in an average time of O(1). At its most basic level, a hash table data structure is just an array. Data is stored into this array at specific indices designated by a hash function. A hash function is a mapping between the set of input data and a set of integers
 
 ## Challenge
 <!-- Description of the challenge -->
+Implement a Hashtable Class with the following methods:
+add, contains, find, hash
 
 ## Approach & Efficiency
 <!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+Big O of time is: O(1)
+Big O of space is: O(n)
 
 ## API
 <!-- Description of each method publicly available in each of your hashtable -->
+- add method:
+    add a value to the hashtable by its key 
+    parameters:
+    key: a string
+    value: any type
+    Arrgument: key and value 
+    return: nothing
+
+
+- find method:
+    this function will search in the hashtable about the key and return the value
+    parameters:
+    key: a string
+    return: the value 
+
+
+- contains method: 
+   this function will check if the there is a value for the key 
+    parameters:
+    key: a string
+    return: a boolean
+    
--- hash_table/hash_table_1.py
+++ hash_table/hash_table_1.py
@@ -1,17 +1,75 @@
+class Node:
+    def __init__(self, value=""):
+        self.value = value
+        self.next = None
+
+    def __add__(self, other):
+        return Node(self.value + other.value)
+
+    def __str__(self):
+        return str(self.value)
+
+
+class LinkedList():
+
+    def __init__(self):
+        self.head = None
+
+    def insert(self, value):
+        node = Node(value)
+
+        if self.head:
+            node.next = self.head
+        self.head = node
+
+    def includes(self,vlaue):
+        current=self.head
+        while current :
+            if vlaue ==current.value[0]:
+                return True
+            current=current.next
+        return False
+
+    def append(self,value):
+        new_node=Node(value)
+        if self.head == None:
+            self.head = new_node
+        else:
+            current=self.head
+            while current.next:
+                current=current.next
+            current.next=new_node
+
+
+
+
 class HashTable :
-    def __init__(self, size = 750):
+
+    def __init__(self, size = 1024):
         self.size = size
-        self._buckets = [None] * self.size
+        self._buckets = [None] *self.size
 
     def hash(self,key:str)->int:
         value=0
         for x in key:
-            value += ord(x) # ord () function accepts a string of unit length as an argument and returns the Unicode equivalence of the passed argument.
-            print(ord(x))
+            value += ord(x)
         index = (value * 7) % self.size   
-        # print(index)
+        return index
 
-if __name__ == "__main__":
-    x=HashTable(34)
-    x.hash("2222222222")
+    def add(self,key,value):
+        index = self.hash(key) 
+        if not self._buckets[index]:
+            self._buckets[index] = LinkedList()
+        self._buckets[index].append([key,value])
+
+    def find(self,key):
+        index = self.hash[key]
+        return index
+
+    def contains(self,key):    
+        index=self.hash(key)
+        if self._buckets[index]:
+            return self._buckets[index].includes(key)
+        else:
+            return False
 
