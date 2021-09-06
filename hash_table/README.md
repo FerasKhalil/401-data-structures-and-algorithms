fd96b9734a31ce5832099236ee27968ae7e999ce
FIRAS AHMAD HASAN <x.firashasan@gmail.com>
2021-09-06 19:16:46 +0300

    fixed repeated word 31

--- hash_table/README.md
+++ hash_table/README.md
@@ -36,3 +36,22 @@ parameters:
 key: a string
 return: a boolean
 
+### repeated word code challenge 31
+
+# Challenge Summary
+<!-- Description of the challenge -->
+- You need to write a function that takes in a string and returns the first word that is repeated at least once.
+
+## Whiteboard Process
+<!-- Embedded whiteboard image -->
+![Repeated word](whiteboard/repeated_word.jpg)
+
+## Approach & Efficiency
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+Big O of time is: O(n)
+Big O of space is: O(1)
+
+## Solution
+<!-- Show how to run your code, and examples of it in action -->
+[Code Source](hash_table_1.py)
+[Code Tests Source](test_hash_table_1.py)
--- hash_table/test_hash_table_1.py
+++ hash_table/test_hash_table_1.py
@@ -1,8 +1,5 @@
 from hash_table.hash_table_1 import *
 
-
-
-
 # def test_add():
 #     hash_table=HashTable()
 #     hash_table.add('first hasher',412)
@@ -28,3 +25,29 @@ from hash_table.hash_table_1 import *
 #     hasher_1=hash_table.hash("firas")
 #     hasher_2=hash_table.hash("firas")
 #     assert hasher_1 == hasher_2
+
+def test_repeated_word_1():
+    stringer = "Once upon a time, there was a brave princess who..."
+    actual = repeated_word(stringer)
+    expected = 'a'
+    assert actual == expected
+
+def test_repeated_word_2():
+    stringer = "It was a queer, sultry summer, the summer they electrocuted the Rosenbergs, and I didn’t know what I was doing in New York..."
+    actual = repeated_word(stringer)
+    expected = "was"
+    assert actual == expected
+
+
+
+def test_repeated_word_3():
+    stringer = "it was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only..."
+    actual = repeated_word(stringer)
+    expected = "it"
+    assert actual == expected
+
+def test_no_repeated_word():
+    stringer = "My tea's gone cold and I'm wondering why I got out of bed at all"
+    actual = repeated_word(stringer)
+    expected = None
+    assert expected == actual
